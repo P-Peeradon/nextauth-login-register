8b6e2c1d708f3ee7e59bc33aacf5d832e1ce45bf
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-24 16:56:43 +1100

    Login Page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 
-function RegisterPage() {
+function LoginPage() {
     return (
         <div>
             <Navbar />
@@ -11,15 +11,14 @@ function RegisterPage() {
                 <h3>Register Page</h3>
                 <hr className='my-3' />
                 <form action="">
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="text" placeholder='Enter your name' />
                     <input className='block bg-gray-300 p-2 my-2 rounded-md' type="email" placeholder='Enter your email' />
                     <input className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Enter your password' />
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Confirm your password' />
-                    <button type="submit" className='bg-green-500 p-2 rounded-md'>Sign Up</button>
+                    
+                    <button type="submit" className='bg-green-500 p-2 rounded-md'>Sign In</button>
                 </form>
             </div>
         </div>
     )
 }
 
-export default RegisterPage
+export default LoginPage
