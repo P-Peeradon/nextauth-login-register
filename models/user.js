04c140f9e904d2cedd51ac877d9f073aaccb1c73
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-24 17:33:56 +1100

    Set up schema.

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            required: false,
+            default: "user"
+        }
+    }
+);
+
+// If schema DNE, create new or use the existing.
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
