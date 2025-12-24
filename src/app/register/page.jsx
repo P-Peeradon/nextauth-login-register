f43d6f560b5d4750af07fe2936b8e3556b809e27
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-24 17:09:25 +1100

    Client side validation

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -5,21 +5,51 @@ import Navbar from '../components/Navbar'
 import Link from 'next/link'
 
 function RegisterPage() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("Password do not match!");
+            return;
+        }
+
+        if (!name || !email || !password || !confirmPassword) {
+            setError("Please complete all inputs");
+            return;
+        }
+
+        
+    }
+
     return (
         <div>
             <Navbar />
             <div className='container mx-auto py-5'>
                 <h3>Register Page</h3>
                 <hr className='my-3' />
-                <form action="">
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="text" placeholder='Enter your name' />
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="email" placeholder='Enter your email' />
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Enter your password' />
-                    <input className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Confirm your password' />
+                <form onSubmit={handleSubmit}>
+
+                    {error && (
+                        <div className='bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2'>
+                            {error}
+                        </div>
+                    )}
+
+                    <input onChange={(e) => setName(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="text" placeholder='Enter your name' />
+                    <input onChange={(e) => setEmail(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="email" placeholder='Enter your email' />
+                    <input onChange={(e) => setPassword(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Enter your password' />
+                    <input onChange={(e) => setConfirmPassword(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Confirm your password' />
                     <button type="submit" className='bg-green-500 p-2 rounded-md'>Sign Up</button>
                 </form>
                 <hr className="my-3" />
-                <p>
+                <p> 
                     Already have an account? Go to <Link href="/login" className='text-blue-500 hover:underline'>Login</Link> Page
                 </p>
             </div>
